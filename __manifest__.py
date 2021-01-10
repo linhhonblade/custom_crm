c5216b4f22e9bd59fb4d40d9eb3057899b48b7fb
linhhonblade <meomaimaimeo@gmail.com>
2021-01-10 17:02:17 +0700

    custom crm module for aht

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "custom_crm",
+
+    'summary': """
+        Custom module crm for aht""",
+
+    'description': """
+        Allow salesperson can search contact to prevent duplicate
+    """,
+
+    'author': "linhhonblade",
+    'website': "https://github.com/linhhonblade",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales/CRM',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+                'contacts'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/contact_views.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
